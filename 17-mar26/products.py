46570e8eb21fa7840fce0d1adde4f1455113e582
WilsonPsychedelic <michaelbrooks452@gmail.com>
2026-03-26 19:52:26 -0700

    added points & products

---
+++ 17-mar26/products.py
@@ -0,0 +1,49 @@
+# Operator Overloading
+# Arithmetic: + (__add__), - (__sub__), = (__mul__), / (__truediv__)
+# Comparison: == (__eq__), != (__ne__), < (__lt__), > (__gt__)
+
+class Product:
+
+    # constructor
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    # Operator(s) overloading
+    def __add__(self, other): # +
+        #return self.price + other.price
+        if isinstance(other, Product):
+            return Product(f"{self.name}+{other.name}", self.price + other.price)
+        return Product("Total", self.price + other)
+
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        return self.__add__()
+    
+    def __mul__(self, number):
+        return Product(f"{number} {self.name}s", self.price * number)
+
+    # CLI representation
+    def __str__(self):
+        """user friendly"""
+        return f"{self.name}: ${self.price}"
+    
+    def __repr__(self):
+        """developer friendly"""
+        return self .__str__()
+
+apple = Product("Apple", 2.55)
+orange = Product("Orange", 1.88)
+chips = Product("Lays", 4.99)
+
+print(apple, orange)
+print(apple + orange + chips)
+
+cart = []
+cart.append(apple)
+cart.append(orange)
+cart.append(chips)
+
+print(cart)
+print("Total in Cart: ", sum(cart))
